33aeb9f632027791891569005deb49fc8ff49fd4
meghanroberts <meghanroberts@bren.ucsb.edu>
2024-04-23 11:29:45 -0700

    deleted extraneous info

--- myapp/functions/costben.R
+++ myapp/functions/costben.R
@@ -180,32 +180,7 @@ unit_cost_dumbell_chart <- #plot for avg per acre or per mile
           axis.title.x = element_text(size = 16, margin = margin(t = 15)),
           plot.title = element_text(size = 20, margin = margin(b = 15)))
 
-  
-  
-  # ggplot(data = data, aes(group = rst_typ)) +
-  #   # create dumbbells ----
-  # geom_segment(aes(x = ttl_lw_, xend = ttl_pp_,
-  #                  y = fct_reorder(sbbsn_n, ttl_pp_), 
-  #                  yend = sbbsn_n),
-  #              color = "grey",
-  #              size = 1.5
-  # ) + # reorder occupation by avg_salary here
-  #   geom_point(aes(x = ttl_lw_, y = sbbsn_n, 
-  #                  color = rst_typ, group = rst_typ), size = 3, color = "#28AFB0") +
-  #   geom_point(aes(x = ttl_pp_, y = sbbsn_n,
-  #                  color = rst_typ, group = rst_typ), size = 3, color = "#03045E") +
-  #   # axis breaks & $ labels ----
-  # scale_x_continuous(labels = scales::label_dollar(scale = 0.000001, suffix = "M")) +
-  #   # pushing y axis labels to edhe of data 
-  #   labs(x = "Total Cost ($ million)",
-  #        y = "",
-  #        title = paste(input, "Habitat Restoration Costs")) +
-  #   theme_minimal() +
-  #   theme(plot.title.position = "plot",
-  #         axis.text.x = element_text(size = 14),  
-  #         axis.text.y = element_text(size = 14),
-  #         axis.title.x = element_text(size = 16, margin = margin(t = 15)),
-  #         plot.title = element_text(size = 20, margin = margin(b = 15)))
+
 }
 
 
